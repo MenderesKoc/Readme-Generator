5c4d74b34e786e46c40b57c48e824e43c17fbdea
mndrs <mndrs.kc@gmail.com>
2022-02-13 22:14:31 -0500

    created file run the test

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,57 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(data) {
+  if (!data.license) {
+    return '';
+  }
+  return `
+  ## License
+  ![licenseBadge](https://img.shields.io/badge/License-${data.license}-blue.svg)
+  ${data.license}
+  [Full license information](${data.licenseLink}).
+  `;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ${renderLicenseSection(data)}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [Tests](#tests)
+  - [Questions](#contact)
+  
+  
+  <a name="installation"></a>
+  ## Installation
+  ${data.installation}
+  
+  <a name="usage"></a>
+  ## Usage
+  ${data.usage}
+  
+  <a name="credits"></a>
+  ## Credits
+  ${data.credits}
+  <a name="tests"></a>
+  ## Tests
+  ${data.tests}
+  
+  <a name="contact"></a>
+  ## Questions 
+  Contact for questions and suggestions: 
+  ${data.name}
+  [email](mailto:${data.email})
+  [GitHub](https://github.com/${data.github})
 `;
 }
+
+module.exports = generateMarkdown;
 
 module.exports = generateMarkdown;
